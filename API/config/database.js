23998ee50749166daf1db005911a517075947e8e
Meet Bavisha <162316999+meet1414@users.noreply.github.com>
2025-05-17 13:37:32 +0530

    Update database.js

--- API/config/database.js
+++ API/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://5997siddhikhunt:siddhi1234@cluster0.9sgbj.mongodb.net/task-management');
+    const conn = await mongoose.connect('mongodb+srv://6968meetbavisha:meetbavisha123@cluster0.ds2ps78.mongodb.net/admin-panel');
     console.log('MongoDB Connected');
     return conn;
   } catch (error) {
